979ef35e0ec240f5a7038cacc6f58d49d82beb8a
yx <yx@yx.com>
2020-08-05 18:16:01 +0800

    te

--- examples/views/Home.vue
+++ examples/views/Home.vue
@@ -3,14 +3,21 @@
         <!-- <img alt='Vue logo' src='../assets/logo.png' /> -->
         <h1>11111</h1>
         <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-        <HelloWorld msg='awesome hhh'/>
+        <HelloWorld msg='awesome hhh' />
+        <div class='container'></div>
     </div>
 </template>
 
 <script>
-
 export default {
     name: 'Home',
     components: {},
 }
 </script>
+<style lang="less" scoped>
+.container {
+    width: 800px;
+    height: 300px;
+    background-color: lightgreen;
+}
+</style>
